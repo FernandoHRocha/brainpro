df11e759d1c7f6e42b474a0a3a3a3c33b480cae9
Fernando Rocha <fhrlobacz@gmail.com>
2021-08-19 13:40:29 -0300

    Refazendo o cabecalho

--- src/componentes/home.jsx
+++ src/componentes/home.jsx
@@ -1,29 +1,34 @@
 function Main(){
     return (
         <header>
-            <nav className="cabecalho">
+            <nav className="cabecalho">   
                 <a href="https://www.brainpro.com.br/" className="cabecalho__logo">
                     <img src="https://cdn.awsli.com.br/400x300/1628/1628166/logo/25350005a0.png" alt="BRAINPRO TECNOLOGIA" />
                 </a>
-                <form action="/buscar" className="busca" method='get'>
-                    <input type="text" className="cabecalho__busca" placeholder='Olá, digite o que você procura...'/>
-                </form>
-                <div className="cabecalho__opcoes">
-                    <div className="cabecalho__opcao">
-                        😀
-                        <p>Minha Conta</p>
-                    </div>
-                    <div className="cabecalho__opcao">
-                        ❤
-                        <p>Favoritos</p>
-                    </div>
-                    <div className="cabecalho__opcao">
-                        ❔
-                        <p>Atendimento</p>
+                <div className="utilidades">
+                    <form action="/buscar" className="cabecalho__busca" method='get'>
+                        <input type="text" className="cabecalho__busca" placeholder='Olá, digite o que você procura...'/>
+                    </form>
+                    <div className="cabecalho__opcoes">
+                        <div className="cabecalho__opcao">
+                            😀
+                            <p>MINHA CONTA</p>
+                        </div>
+                        <div className="cabecalho__opcao">
+                            ❤
+                            <p>FAVORITOS</p>
+                        </div>
+                        <div className="cabecalho__opcao">
+                            ❔
+                            <p>ATENDIMENTO</p>
+                        </div>
+                        <div className="cabecalho__opcao">
+                            🏎
+                            <p>CARRINHO</p>
+                        </div>
                     </div>
                 </div>
             </nav>
-
         </header>
     )
 }
